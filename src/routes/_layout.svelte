63c0eaba3f2eed9457437ac7cc0523de651d5706
esinx <me@esinx.net>
2020-09-28 17:55:57 +0900

    page transitions

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,9 @@
 <script>
+  import { stores, goto } from "@sapper/app";
+  import { fade, fly } from "svelte/transition";
   import Nav from "#components/Nav.svelte";
+
+  const { page, preloading, session } = stores();
 </script>
 
 <style lang="scss" global>
@@ -17,5 +21,13 @@
 <Nav />
 
 <main>
-  <slot />
+  {#if $preloading}
+
+  {:else}
+    <div
+      in:fly={{ y: 200, duration: 500 }}
+      out:fly={{ y: -200, duration: 500 }}>
+      <slot />
+    </div>
+  {/if}
 </main>
