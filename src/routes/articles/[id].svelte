683c4ebee454ad9639a12acbc4f8e2c2549c2185
Eunsoo Shin <me@esinx.net>
2021-01-03 13:43:27 +0900

    naming change

---
+++ src/routes/articles/[id].svelte
@@ -0,0 +1,43 @@
+<script context="module">
+	export async function preload({ params, query }) {
+		const { id } = params;
+		const res = await this.fetch(`/data/article/${id}`);
+		const data = await res.json();
+		return {
+			article: data,
+		};
+	}
+</script>
+
+<script>
+	export let article;
+
+	$: datetext = new Date(article.frontmatter.date).toDateString();
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="/victor-mono/index.css" />
+	<title>{article.frontmatter.title}</title>
+	<meta name="description" content={article.frontmatter.tags.join(', ')} />
+</svelte:head>
+
+<article>
+	<header>
+		<h1 class="title">{article.frontmatter.icon} {article.frontmatter.title}</h1>
+		<div class="subtitle">{article.frontmatter.subtitle}</div>
+		<div class="meta">
+			<div class="tags">
+				{#each article.frontmatter.tags as tag}
+					<div class="tag">{tag}</div>
+				{/each}
+			</div>
+			<div class="date">{datetext}</div>
+		</div>
+	</header>
+	<div class="content">
+		{@html article.html}
+	</div>
+	<footer>
+		<div class="author">an article by <span class="name">Eunsoo Shin (esinx)</span></div>
+	</footer>
+</article>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,128 +1,149 @@
 <script context="module">
-  export async function preload(page, session) {
-    let articles;
-    {
-      const res = await this.fetch("/data/article");
-      articles = await res.json();
-    }
-    return { articles };
-  }
+	export async function preload(page, session) {
+		let articles;
+		{
+			const res = await this.fetch("/data/article");
+			articles = await res.json();
+		}
+		return { articles };
+	}
 </script>
 
 <script>
-  export let articles = [];
-  let reveal_contacts = false;
-  const toDisplayDate = date => new Date(date).toDateString();
+	export let articles = [];
+	let reveal_contacts = false;
+	const toDisplayDate = (date) => {
+		const d = new Date(date);
+		return `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(
+			2,
+			"0"
+		)}-${String(d.getDate()).padStart(2, "0")}`;
+	};
 </script>
 
 <style lang="scss">
-  .root {
-    // width: 100vw;
-    min-height: 100vh;
-    header {
-      display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-      place-items: center;
-      gap: 20px;
-      #pic-of-me {
-        width: 180px;
-        height: 180px;
-        border-radius: 50%;
-        background: url(/you-probably-should-not-save-this.webp) no-repeat
-          center center;
-        background-size: cover;
-      }
-      .description {
-        h1,
-        p {
-          margin: 10px 0;
-          text-align: left;
-        }
-        .links {
-          a {
-            display: inline-flex;
-            align-items: center;
-            color: #000;
-            text-decoration: none;
-            border: solid #000 1px;
-            border-radius: 5px;
-            padding: 5px 10px;
-            &:hover {
-              color: #fff;
-              background: #000;
-            }
-            svg {
-              margin-right: 10px;
-            }
-          }
-        }
-      }
-    }
+	.root {
+		// width: 100vw;
+		min-height: 100vh;
+		header {
+			display: grid;
+			grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+			place-items: center;
+			gap: 20px;
+			#pic-of-me {
+				width: 180px;
+				height: 180px;
+				border-radius: 50%;
+				background: url(/you-probably-should-not-save-this.webp)
+					no-repeat center center;
+				background-size: cover;
+			}
+			.description {
+				h1,
+				p {
+					margin: 10px 0;
+					text-align: left;
+				}
+				.links {
+					a {
+						display: inline-flex;
+						align-items: center;
+						color: #000;
+						text-decoration: none;
+						border: solid #000 1px;
+						border-radius: 5px;
+						padding: 5px 10px;
+						&:hover {
+							color: #fff;
+							background: #000;
+						}
+						svg {
+							margin-right: 10px;
+						}
+					}
+				}
+			}
+		}
 
-    #articles {
-      margin: 50px auto;
-      a.article {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        border: solid #efefef 1px;
-        border-radius: 5px;
-        padding: 10px 20px;
-        text-decoration: none;
-        font-size: 20px;
-        color: inherit;
-        transition: all 0.3s;
-        &:hover {
-          transform: scale(1.05);
-          background: #efefef;
-        }
-        .date {
-          text-align: right;
-        }
-        .tags {
-          .tag {
-            background: #333;
-            color: white;
-            font-size: 12px;
-            padding: 2px 5px;
-            margin: 2px;
-            display: inline-block;
-            border-radius: 2px;
-          }
-        }
-      }
-    }
-    #reveal_contacts {
-      border-radius: 2px;
-      padding: 2px 5px;
-      color: inherit;
-      text-decoration: none;
-      background: #efefef;
-    }
-  }
+		#articles {
+			margin: 50px auto;
+			a.article {
+				display: grid;
+				grid-template-columns: auto 1fr auto;
+				gap: 20px;
+				margin: 20px auto;
+				padding: 10px 20px;
+				border: solid #efefef 1px;
+				border-radius: 5px;
+				text-decoration: none;
+				font-size: 20px;
+				color: inherit;
+				transition: all 0.3s;
+				&:hover {
+					transform: scale(1.05);
+					background: #efefef;
+				}
+				.icon {
+					align-self: center;
+					font-size: 30px;
+					line-height: 30px;
+					width: 30px;
+					height: 30px;
+				}
+				.info {
+					.subtitle {
+						font-size: 12px;
+						color: #777;
+					}
+				}
+				.date {
+					font-size: 15px;
+					text-align: right;
+				}
+				.tags {
+					.tag {
+						background: #333;
+						color: white;
+						font-size: 12px;
+						padding: 2px 5px;
+						margin: 2px;
+						display: inline-block;
+						border-radius: 2px;
+					}
+				}
+			}
+		}
+		#reveal_contacts {
+			border-radius: 2px;
+			padding: 2px 5px;
+			color: inherit;
+			text-decoration: none;
+			background: #efefef;
+		}
+	}
 </style>
 
 <div class="root">
-
-  <header>
-    <div id="pic-of-me" />
-    <div class="description">
-      <h1>Hi. I'm Eunsoo.</h1>
-      <p>I write code and things about code.</p>
-      <p>
-        I overuse JavaScript: NodeJS, Svelte, React
-        <br />
-        I enjoy functional programming, thanks to OCaml.
-      </p>
-      <div class="links">
-        <a href="https://github.com/esinx" target="_blank">
-          <svg
-            viewBox="0 0 15 15"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            width="15"
-            height="15">
-            <path
-              d="M9.358 2.145a8.209 8.209 0 00-3.716
+	<header>
+		<div id="pic-of-me" />
+		<div class="description">
+			<h1>Hi. I'm Eunsoo.</h1>
+			<p>I write code and things about code.</p>
+			<p>
+				I overuse JavaScript: NodeJS, Svelte, React
+				<br />
+				I enjoy functional programming, thanks to OCaml.
+			</p>
+			<div class="links">
+				<a href="https://github.com/esinx" target="_blank">
+					<svg
+						viewBox="0 0 15 15"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						width="15"
+						height="15">
+						<path
+							d="M9.358 2.145a8.209 8.209 0 00-3.716
               0c-.706-.433-1.245-.632-1.637-.716a2.17 2.17 0 00-.51-.053 1.258
               1.258 0 00-.232.028l-.01.002-.004.002h-.003l.137.481-.137-.48a.5.5
               0 00-.32.276 3.12 3.12 0 00-.159 2.101A3.354 3.354 0 002 5.93c0
@@ -138,81 +159,81 @@
               00-.319-.277l-.137.48c.137-.48.136-.48.135-.48l-.002-.001-.004-.002-.009-.002a.671.671
               0 00-.075-.015 1.261 1.261 0 00-.158-.013 2.172 2.172 0
               00-.51.053c-.391.084-.93.283-1.636.716z"
-              fill="currentColor" />
-          </svg>
-          @esinx
-        </a>
-        <a href="https://twitter.com/e_sinx" target="_blank">
-          <svg
-            viewBox="0 0 15 15"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            width="15"
-            height="15">
-            <path
-              d="M14.977 1.467a.5.5 0 00-.87-.301 2.559 2.559 0
+							fill="currentColor" />
+					</svg>
+					@esinx
+				</a>
+				<a href="https://twitter.com/e_sinx" target="_blank">
+					<svg
+						viewBox="0 0 15 15"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						width="15"
+						height="15">
+						<path
+							d="M14.977 1.467a.5.5 0 00-.87-.301 2.559 2.559 0
               01-1.226.763A3.441 3.441 0 0010.526 1a3.539 3.539 0 00-3.537
               3.541v.44C3.998 4.75 2.4 2.477 1.967 1.325a.5.5 0
               00-.916-.048C.004 3.373-.157 5.407.604 7.139 1.27 8.656 2.61 9.864
               4.51 10.665 3.647 11.276 2.194 12 .5 12a.5.5 0 00-.278.916C1.847
               14 3.55 14 5.132 14h.048c4.861 0 8.8-3.946
               8.8-8.812v-.479c.363-.37.646-.747.82-1.236.193-.546.232-1.178.177-2.006z"
-              fill="currentColor" />
-          </svg>
-          @e_sinx
-        </a>
-      </div>
-      <p>
-        I have a
-        <a href="/resume">résumé page</a>
-      </p>
-      <p style="color:#ccc;">
-        These icons are teenyicons and the font is IBM Plex Mono. Both awesome
-        works.
-      </p>
-    </div>
-  </header>
+							fill="currentColor" />
+					</svg>
+					@e_sinx
+				</a>
+			</div>
+			<p>I have a <a href="/resume">résumé page</a></p>
+			<p style="color:#ccc;">
+				These icons are teenyicons and the font is IBM Plex Mono. Both
+				awesome works.
+			</p>
+		</div>
+	</header>
 
-  <div id="articles">
-    <h1>🖋 Articles</h1>
-    {#each articles as article}
-      <a class="article" href={`/article/${article.slug}`}>
-        <span class="title">{article.title}</span>
-        <span class="date">{toDisplayDate(article.date)}</span>
-        <span class="tags">
-          {#each article.tags as tag}
-            <span class="tag">{tag}</span>
-          {/each}
-        </span>
-      </a>
-    {/each}
-  </div>
-  <p>
-    By the way, this blog is still under construction. Aren't my page
-    transitions sick? It's
-    <a href="https://sapper.svelte.dev/" target="_blank">sapper</a>
-    magic
-  </p>
+	<div id="articles">
+		<h1>🖋 Articles</h1>
+		{#each articles as article}
+			<a class="article" href={`/articles/${article.slug}`}>
+				<div class="icon">{article.icon}</div>
+				<div class="info">
+					<div class="title">{article.title}</div>
+					<div class="subtitle">{article.subtitle}</div>
+					<div class="tags">
+						{#each article.tags as tag}
+							<span class="tag">{tag}</span>
+						{/each}
+					</div>
+				</div>
+				<div class="date">{toDisplayDate(article.date)}</div>
+			</a>
+		{/each}
+	</div>
+	<p>
+		By the way, this blog is still under construction. Aren't my page
+		transitions sick? It's
+		<a href="https://sapper.svelte.dev/" target="_blank">sapper</a>
+		magic
+	</p>
 
-  <div id="contact">
-    <h1>📬 Contact</h1>
-    <p>
-      Hey spambots,
-      <a
-        id="reveal_contacts"
-        href="javascript:void(0)"
-        on:click={() => {
-          reveal_contacts = true;
-        }}>
-        click me
-      </a>
-      to reveal my online mail address
-    </p>
-    {#if reveal_contacts}
-      <div id="contact-grid">
-        <div>📧 me@esinx.net</div>
-      </div>
-    {/if}
-  </div>
-
+	<div id="contact">
+		<h1>📬 Contact</h1>
+		<p>
+			Hey spambots,
+			<a
+				id="reveal_contacts"
+				href="javascript:void(0)"
+				on:click={() => {
+					reveal_contacts = true;
+				}}>
+				click me
+			</a>
+			to reveal my online mail address
+		</p>
+		{#if reveal_contacts}
+			<div id="contact-grid">
+				<div>📧 me@esinx.net</div>
+			</div>
+		{/if}
+	</div>
 </div>
